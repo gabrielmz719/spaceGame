0d6200737c97dd81e4ecd3e849fa97d3f4173c96
Gabriel Mateus Zenerato <g20998986@gmail.com>
2025-06-14 19:09:53 +0000

    Criei os eixos o tabuleiro

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-alert("Teste");
+const tileSize = 32;
+const rows = 16;
+const collumns = 16;
+
+
+let board;
+let boardWidth = tileSize * collumns;
+let boardHeight = tileSize * rows;
+let context;
+
+let shipWidth = tileSize * 2;
+let shipHeight = tileSize;
+let shipx = tileSize * collumns/2 -tileSize
+let shipy = tileSize * rows - tileSize * 2; 
+
+let ship = {
+    x : shipx,
+    y : shipy,
+    width : shipWidth,
+    height : shipHeight
+}
+
+window.onload = function(){
+    board = document.getElementById('board');
+    board.width=boardWidth;
+    board.height = boardHeight;
+    context=board.getContext("2d");
+}
